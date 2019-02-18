0ef7ac9eeee0b382bc39611af0bf790c88d5aeed
Andrey Gridyaev <a.gridyaev@semrush.com>
2019-02-18 06:16:42 -0500

    add simple realization for "Add" and "Print" methods

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <set>
+#include <iomanip>
 
 using namespace std;
 
@@ -14,30 +17,77 @@ public:
   int GetDay() const {
 	  return day;
   };
+
+  void SetYear(int new_year) {
+	  year = new_year;
+  }
+
+  void SetMonth(int new_month) {
+	  month = new_month;
+  }
+
+  void SetDay(int new_day) {
+	  day = new_day;
+  }
 private:
   int year, month, day;
 };
 
 bool operator<(const Date& lhs, const Date& rhs) {
-	if (lhs.year == rhs.year) {
-		if (lhs.month == rhs.month) {
-			return lhs.day < rhs.day;
+	if (lhs.GetYear() == rhs.GetYear()) {
+		if (lhs.GetMonth() == rhs.GetMonth()) {
+			return lhs.GetDay() < rhs.GetDay();
 		}
-		return lhs.month < rhs.month;
+		return lhs.GetMonth() < rhs.GetMonth();
 	}
-	return lhs.year < rhs.year;
+	return lhs.GetYear() < rhs.GetYear();
+};
+
+istream& operator>>(istream& stream, Date& date) {
+	int new_year, new_month, new_day;
+	stream >> new_year;
+	date.SetYear(new_year);
+	stream.ignore(1);
+
+	stream >> new_month;
+	date.SetMonth(new_month);
+	stream.ignore(1);
+
+	stream >> new_day;
+	date.SetDay(new_day);
+	return stream;
+};
+
+ostream& operator<<(ostream& stream, const Date& date) {
+	stream << setfill('0');
+	stream << setw(4) << date.GetYear() << '-'
+	       << setw(2) << date.GetMonth() << '-'
+		   << setw(2) << date.GetDay();
+	return stream;
 };
 
 class Database {
 public:
-  void AddEvent(const Date& date, const string& event);
+  void AddEvent(const Date& date, const string& event) {
+		  storage[date].insert(event);
+  };
   bool DeleteEvent(const Date& date, const string& event);
   int  DeleteDate(const Date& date);
 
-  /* ??? */ Find(const Date& date) const;
+  void Find(const Date& date) const;
 
-  void Print() const;
+  void Print() const {
+	  for (const auto& record : storage) {
+		  for (const auto& event : record.second) {
+			  cout << record.first << " " << event << endl;
+		  }
+	  }
+  };
+
+private:
+  map<Date, set<string>> storage;
 };
+
 
 int main() {
   Database db;
@@ -45,6 +95,24 @@ int main() {
   string command;
   while (getline(cin, command)) {
 
+	  cin >> command;
+	  if (!command.length()) {
+		  continue;
+	  } else {
+		  if (command == "Add") {
+			  Date date;
+			  string event;
+
+			  cin >> date;
+			  cin >> event;
+
+			  db.AddEvent(date, event);
+		  }
+
+		  if (command == "Print") {
+			  db.Print();
+		  }
+	  }
   }
 
   return 0;
