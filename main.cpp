a7e217f70fec12150b7e0016106957f86e7a280b
Andrey Gridyaev <a.gridyaev@semrush.com>
2019-02-18 04:29:03 -0500

    add empty project template

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class Date {
+public:
+  int GetYear() const;
+  int GetMonth() const;
+  int GetDay() const;
+};
+
+bool operator<(const Date& lhs, const Date& rhs);
+
+class Database {
+public:
+  void AddEvent(const Date& date, const string& event);
+  bool DeleteEvent(const Date& date, const string& event);
+  int  DeleteDate(const Date& date);
+
+  /* ??? */ Find(const Date& date) const;
+
+  void Print() const;
+};
+
 int main() {
-	return 0;
+  Database db;
+
+  string command;
+  while (getline(cin, command)) {
+
+  }
+
+  return 0;
 }
