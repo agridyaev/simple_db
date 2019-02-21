08ca630a8da72629cb1b290727adaa89467c4796
Andrey Gridyaev <a.gridyaev@semrush.com>
2019-02-21 09:41:47 -0500

    rename "input" variable to remove shadowing name from outer scope

--- test_simple_db.py
+++ test_simple_db.py
@@ -7,24 +7,24 @@ from simple_db_test_data import UNKNOWN_COMMAND, \
                                 OPERATIONS
 
 
-def run_simple_db(simple_db_path, input):
+def run_simple_db(simple_db_path, test_input):
     simple_db_proc = subprocess.run(simple_db_path,
                                     capture_output=True,
-                                    input=input,
+                                    input=test_input,
                                     text=True)
     return simple_db_proc.stdout
 
 
-@pytest.mark.parametrize("input, expected_out", UNKNOWN_COMMAND)
-def test_unknown_command(request, input, expected_out):
+@pytest.mark.parametrize("test_input, expected_out", UNKNOWN_COMMAND)
+def test_unknown_command(request, test_input, expected_out):
     """Если пользователь ввёл неизвестную команду, то программа должна об этом сообщить,
        выведя строку «Unknown command: COMMAND»
     """
-    assert run_simple_db(request.config.getoption('--simple-db-path'), input) == expected_out
+    assert run_simple_db(request.config.getoption('--simple-db-path'), test_input) == expected_out
 
 
-@pytest.mark.parametrize("input, expected_out", DATE_WRONG_FORMAT)
-def test_date_wrong_format(request, input, expected_out):
+@pytest.mark.parametrize("test_input, expected_out", DATE_WRONG_FORMAT)
+def test_date_wrong_format(request, test_input, expected_out):
     """Если дата не соответствует формату Год-Месяц-День, где Год, Месяц и День — произвольные целые числа,
        то программа должна напечатать «Wrong date format: DATE», где DATE — это то,
        что пользователь ввёл вместо даты (до пробела).
@@ -33,11 +33,11 @@ def test_date_wrong_format(request, input, expected_out):
        лишних символов ни перед годом, ни после дня.
 
        Части даты не могут быть пустыми, но могут быть нулевыми и даже отрицательными."""
-    assert run_simple_db(request.config.getoption('--simple-db-path'), input) == expected_out
+    assert run_simple_db(request.config.getoption('--simple-db-path'), test_input) == expected_out
 
 
-@pytest.mark.parametrize("input, expected_out", DATE_INVALID)
-def test_date_invalid(request, input, expected_out):
+@pytest.mark.parametrize("test_input, expected_out", DATE_INVALID)
+def test_date_invalid(request, test_input, expected_out):
     """Если формат даты верен, необходимо проверить валидность месяца и дня.
 
        Если номер месяца не является числом от 1 до 12, выведите «Month value is invalid: MONTH»,
@@ -55,9 +55,9 @@ def test_date_invalid(request, input, expected_out):
 
        Если неверны как месяц, так и день, то нужно вывести одно сообщение об ошибке в месяце.
     """
-    assert run_simple_db(request.config.getoption('--simple-db-path'), input) == expected_out
+    assert run_simple_db(request.config.getoption('--simple-db-path'), test_input) == expected_out
 
 
-@pytest.mark.parametrize("input, expected_out", OPERATIONS)
-def test_operations(request, input, expected_out):
-    assert run_simple_db(request.config.getoption('--simple-db-path'), input).rstrip() == expected_out
+@pytest.mark.parametrize("test_input, expected_out", OPERATIONS)
+def test_operations(request, test_input, expected_out):
+    assert run_simple_db(request.config.getoption('--simple-db-path'), test_input).rstrip() == expected_out
